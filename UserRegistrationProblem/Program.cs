f5c49739d9b1c9f52baffc30dc8b1c3f45d2ff02
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-08-02 16:53:06 +0530

    [Riya] Add : Validation of First Name

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+namespace UserRegistrationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validate validate = new Validate();
+            Console.WriteLine("Enter the first name to validate:");
+            string FirstName = Console.ReadLine();
+            validate.FirstName(FirstName);
+        }
+    }
+}
