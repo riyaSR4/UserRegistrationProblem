b75f8fa3cb0107bdcb4134f852b31ac8d399b36a
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-08-02 21:52:40 +0530

    [Riya] Add : Validation of Minimum Length of Character

--- UserRegistrationProblem/Validate.cs
+++ UserRegistrationProblem/Validate.cs
@@ -13,6 +13,7 @@ namespace UserRegistrationProblem
         string lastName = "^[A-Z]{1}[a-z]{2,}$";
         string email = "^[a-z]+[._+-]{0,1}[a-z]+[@]{1}[a-z]+[.]{1}[a-z]{2,3}([.]{1}[a-z]{2}){0,1}$";
         string phoneNumber = "^[0-9]{2}[ ]{1}[0-9]{10}$";
+        string character = "^.{8,}$";
         public void FirstName(string input)
         {
             bool result = Regex.IsMatch(input, firstName);
@@ -40,6 +41,14 @@ namespace UserRegistrationProblem
         public void PhoneNumber(string input)
         {
             bool result = Regex.IsMatch(input, phoneNumber);
+            if (result)
+                Console.WriteLine("Validation Successful");
+            else
+                Console.WriteLine("Validation Unsuccessful");
+        }
+        public void CheckCharacter(string input)
+        {
+            bool result = Regex.IsMatch(input, character);
             if (result)
                 Console.WriteLine("Validation Successful");
             else
